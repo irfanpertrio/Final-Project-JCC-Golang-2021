7eb4c0d268e2c112c7e6b56c418d9a1882a104d0
Petagonest <81139813+Petagonest@users.noreply.github.com>
2021-09-27 03:35:52 +0700

    Update_data

--- config/config.go
+++ config/config.go
@@ -8,21 +8,27 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var (
-	username string = os.Getenv("DB_USERNAME")
-	password string = os.Getenv("DB_PASSWORD")
-	database string = os.Getenv("DB_DATABASE")
-	host     string = os.Getenv("DB_HOST")
-)
-
 const (
 	localUsername string = "root"
-	localPassword string = ""
+	localPassword string = "password"
 	localDatabase string = "db_movie"
 )
 
 // HubToMySQL
 func MySQL() (*sql.DB, error) {
+
+	// errEnv := godotenv.Load()
+	// if errEnv != nil {
+	// 	log.Fatalf("Error on loading .env file")
+	// }
+
+	var (
+		username string = os.Getenv("DB_USERNAME")
+		password string = os.Getenv("DB_PASSWORD")
+		database string = os.Getenv("DB_DATABASE")
+		host     string = os.Getenv("DB_HOST")
+	)
+
 	var dsn string
 
 	if username == "" {
